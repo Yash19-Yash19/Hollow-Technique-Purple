326864ba643a048dc736c7dacd78795b8fc10c28
Yash Liladhar Amarapurkar <73273674+Yash19-Yash19@users.noreply.github.com>
2024-04-07 06:02:24 +0000

    Language translating code (only chat )

---
+++ chat_x/english_x.py
@@ -0,0 +1,17 @@
+# Import the translate function from the mtranslate module
+from mtranslate import translate
+import colorama  # Importing colorama for colorful console outputs
+
+# Initializing colorama for automatic reset of colors
+colorama.init(autoreset=True)
+
+# Define an English phrase to be translated
+english_phrase = "Did you subscribe to my channel?"
+
+# Use the translate function to translate the English phrase to Hindi
+# Parameters: source text, target language code ("hi" for Hindi), source language code ("auto" for automatic detection)
+hindi_trans = translate(english_phrase, "hi", "auto")
+
+# Print the translated phrase
+print("TRANSLATED:", hindi_trans)
+
---
+++ chat_x/hindi_x.py
@@ -0,0 +1,17 @@
+# Import the translate function from the mtranslate module
+from mtranslate import translate
+# from mtranslate import * 
+import colorama  # Importing colorama for colorful console outputs
+
+# Initializing colorama for automatic reset of colors
+colorama.init(autoreset=True)
+
+# Define a Hinglish phrase to be translated
+hinglish_phrase = "kya apne subscribe kiya h mere channel ko"
+
+# Use the translate function to translate the Hinglish phrase to English
+# Parameters: source text, target language code ("en" for English), source language code ("auto" for automatic detection)
+english_trans = translate(hinglish_phrase, "en", "auto")
+
+# Print the translated phrase
+print("TRANSLATED:", english_trans)
